fd4da9935587ef63d61429716b404cc434cd68f8
Jack Stanek <stane064@umn.edu>
2019-04-08 15:50:34 -0500

    Add basic legend

--- src/components/legend.js
+++ src/components/legend.js
@@ -4,31 +4,41 @@ export default class Legend extends Component {
   constructor(props) {
     super(props);
     this.parseLegend = this.parseLegend.bind(this);
+    this.services = this.parseLegend();
   }
 
   componentDidMount() {
   }
 
   parseLegend() {
-    /* Get the letters we use for the points on the map.
-     * TODO: Not the fastest way of doing this. Maybe compute
-     * this only in the map? */
     let services = this.props.data
-      .map(d => d.service)
-      .reduce((a, c) => {
-        if (c.toLowerCase() !== a[a.length - 1].toLowerCase()) {
-          return a.push(c);
-        } else {
+        .map(d => d.service)
+        .sort()
+        .reduce((a, c) => {
+          if (a.length === 0 ||
+              c.toLowerCase() !== a[a.length - 1].toLowerCase()) {
+            a.push(c);
+          }
           return a;
-        }
-      }, []);
+        }, []);
 
     return services;
+  }
+
+  makeTable() {
+    return this.services.map(s =>
+                             <tr>
+                              <td>{s.charAt(0)}</td>
+                              <td>{s}</td>
+                             </tr>);
   }
 
   render() {
     return (
         <div className="Legend">
+         <table className="table">
+          {this.makeTable()}
+         </table>
         </div>
     );
   }
