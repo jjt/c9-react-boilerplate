9bc423c70ef0a411506843ea61e6d08173b596a7
Jack Stanek <stane064@umn.edu>
2019-04-21 22:23:25 -0500

    Finished integrating leaflet

--- src/components/map.js
+++ src/components/map.js
@@ -16,11 +16,35 @@ export default class Map extends Component {
       currentLocation: "create",
       portion: undefined,
       projection: undefined
+
     }
   }
 
   // Type check for data types passed into map.js then create map if correct.
   componentDidMount() {
+    let osmMap =
+        L.map("osm-map",
+              {
+                //dragging: false,
+                zoomControl: false,
+                boxZoom: false,
+                doubleClickZoom: false,
+                //scrollWheelZoom: false,
+                zoomDelta: 0
+              })
+        .setView([44.94, -93.10], 13);
+
+    /* TODO: Don't use OSM's tile server. It is not intended for this. */
+    L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>.',
+      minZoom: 13,
+      maxZoom: 15
+    }).addTo(osmMap);
+
+    let that = this;
+    osmMap.on("zoom", function() { that.updatePointPositions(); });
+    this.osmMap = osmMap;
+
     this.createMap();
   }
 
@@ -30,80 +54,45 @@ export default class Map extends Component {
       if (this.state.currentLocation === "create") {
         this.destroyMap();
         this.createMap(years);
-      } else if (this.state.currentLocation === "update") {
-        this.destroyMap();
-        this.updateMap(years);
       } else {
-        this.selectMap(this.state.portion, this.state.projection, this.props.years);
+        if (this.state.currentLocation === "select") {
+          this.selectMap(this.state.portion, this.state.projection, this.props.years);
+        }
+        this.updateMap(years);
       }
     }
   }
 
   createMap(years) {
-    const that = this;
-    const setupOSM = (features) => {
-      let osmMap =
-          L.map("osm-map",
-                {
-                  //dragging: false,
-                  zoomControl: false,
-                  boxZoom: false,
-                  doubleClickZoom: false,
-                  //scrollWheelZoom: false,
-                  zoomDelta: 0
-                })
-          .setView([44.94, -93.10], 13);
-
-      L.geoJSON(features, {
-        style: (feature) => {
-          return {fillOpacity: 1}
-        }
-      }).addTo(osmMap);
-
-      /* TODO: Don't use OSM's tile server. It is not intended for this. */
-      L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>.',
-        minZoom: 13,
-        maxZoom: 15
-      }).addTo(osmMap);
-
-      osmMap.on("viewreset", that.updateMap);
-      this.osmMap = osmMap;
-    };
-
-    setupOSM(this.props.geodata.features);
-
-    const map = d3.select("#osm-map svg");
-    const data = (years !== undefined) ? years : this.props.data;
-    const amountAllocated = this.districtAllocated(data);
-
-    let sum = 0;
-
-    for (let i = 0; i < 17; i++) {
-      sum += amountAllocated[i].value;
-    }
-
-    const formatter = new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD',
-    });
+    L.geoJSON(this.props.geodata.features, {
+      style: (feature) => {
+        return {fillOpacity: 1}
+      }
+    }).addTo(this.osmMap);
 
     d3.select("#osm-map").selectAll("path")
       .classed("map-piece", true)
       .data(this.props.geodata.features);
-    this.updateMap();
+
+    this.updateMap(years);
+  }
+
+  updatePointPositions() {
+    let osmMap = this.osmMap;
+    let g = d3.select("#osm-map").select('g');
+    g.selectAll(".map-point")
+      .attr("cx", (d, i) =>
+            osmMap.latLngToLayerPoint({lat: d.latitude, lon: d.longitude}).x)
+      .attr("cy", (d, i) =>
+            osmMap.latLngToLayerPoint({lat: d.latitude, lon: d.longitude}).y)
+      .attr("r", 10);
   }
 
   updateMap(years) {
     const that = this;
-    const map = d3.select("#osm-map svg");
-    const bounds = map.node().getBoundingClientRect();;
     const projection = d3.geoMercator().fitSize([800, 800], this.props.geodata);
-    const path = d3.geoPath().projection(projection);
     const data = (years !== undefined) ? years : this.props.data;
     const amountAllocated = this.districtAllocated(data);
-    const infobox = d3.select(".infobox");
-    let selectedIndex = -1;
 
     const min_max = d3.extent(amountAllocated, function(d) {
       if (d.name !== "Citywide") {
@@ -114,15 +103,8 @@ export default class Map extends Component {
     const colorScale  = d3.scaleLinear().domain([0,min_max[1]])
     .range(["white", "blue"]);
 
-    const formatter = new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD',
-    });
-
     d3.selectAll("#osm-map path")
       .on("click", function(d,i){
-        selectedIndex = i;
-        let centroid = path.centroid(d);
         that.setState({ currentLocation: "select", portion: d, projection:projection });
         that.selectMap(d, projection, that.props.years);
         d3.select("body").on("keydown", function() {
@@ -131,7 +113,6 @@ export default class Map extends Component {
     	      .classed("infobox-hidden", true);
             that.unSelectMap();
             that.setState({ currentLocation: "update", portion: undefined, projection:undefined });
-            selectedIndex = -1;
           }
         });
       })
@@ -151,8 +132,6 @@ export default class Map extends Component {
 
   // Set other paths to lower opacity.
   selectMap(portion, projection, year) {
-    let that = this;
-    const bounds = d3.select(".Map svg").node().getBoundingClientRect();;
     const improvementsScale = d3.scaleOrdinal()
       .domain(['Community Facilities', 'Internal Service', 'Streets and Utilities', 'Residential and Economic Development'])
       .range(['#a6cee3', '#1f78b4', '#b2df8a', '#33a02c']);
@@ -172,32 +151,29 @@ export default class Map extends Component {
     g.selectAll('.map-point').remove();
     g.selectAll('.map-text').remove();
 
-    let pts = g.selectAll('circle')
-        .data(districtPoints)
-        .enter().append('circle')
-        .classed("map-point", true)
-        .attr("cx", (d, i) => that.osmMap.latLngToLayerPoint({lat: d.latitude, lon: d.longitude}).x)
-        .attr("cy", (d, i) => that.osmMap.latLngToLayerPoint({lat: d.latitude, lon: d.longitude}).y)
-        .attr("r", 7.5)
-        .attr("fill", function(d,i) {
-          return improvementsScale(d.service);
-        })
-        .on("click", function(d,i){
-          infobox.html("<p> Title: " + d.title + "</p>" +
-                       "<p> Department: " + d.department + "</p>" +
-                       "<p> Amount: " + formatter.format(d.amount) + "</p>" +
-                       "<p> District: " + d.district + "</p>" +
-                       "<p> Year: " + d.year + "</p>" +
-                       "<p> Location: " + d.location + "</p>" +
-                       "<p> Description: " + d.description + "</p>")
-            .classed("infobox-hidden", false);
-        });
+    g.selectAll('circle')
+      .data(districtPoints)
+      .enter().append('circle')
+      .classed("map-point", true)
+      .attr("r", 7.5)
+      .attr("fill", function(d,i) {
+        return improvementsScale(d.service);
+      })
+      .on("click", function(d,i){
+        infobox.html("<p> Title: " + d.title + "</p>" +
+                     "<p> Department: " + d.department + "</p>" +
+                     "<p> Amount: " + formatter.format(d.amount) + "</p>" +
+                     "<p> District: " + d.district + "</p>" +
+                     "<p> Year: " + d.year + "</p>" +
+                     "<p> Location: " + d.location + "</p>" +
+                     "<p> Description: " + d.description + "</p>")
+          .classed("infobox-hidden", false);
+      });
+    this.updatePointPositions();
   }
 
   destroyMap() {
-    const svg = d3.select(".Map svg");
-    let g = svg.selectAll("g").transition().style("opacity", 0);
-    g.remove();
+    d3.selectAll("#osm-map path").remove();
   }
 
   colorScale(g, min_max) {
